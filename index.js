16540a9bb4aa86b421cea313b3d1648de9043876
Zachary Sturbaum <zacharysturbaum@gmail.com>
2018-01-14 22:37:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,3 +16,18 @@ function destructivelyRemoveLastKitten(name) {
 function destructivelyRemoveFirstKitten(name) {
   kittens.shift(name)
 }
+function appendKitten(name) {
+  return [...kittens, name]
+}
+
+function prependKitten(name) {
+  return [name, ...kittens]
+}
+
+function removeFirstKitten() {
+  return kittens.slice(1)
+}
+
+function removeLastKitten() {
+  return kittens.slice(0, kittens.length - 1)
+}
